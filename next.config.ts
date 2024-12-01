8d61e0f73cb11a2c0729a9d11a9f5aa8b16e8960
Micah Borghese <micahborghese@gmail.com>
2024-12-01 15:54:29 -0600

    implement redirects

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/pitch_deck',
+        destination: 'https://drive.google.com/file/d/1XbCJU2JZWGG0IoPcVB5_EieFHwrlbIYS/view',
+        permanent: false, // Use true for permanent (308) redirect
+      },
+      {
+        source: '/sponsor_info',
+        destination: 'https://drive.google.com/file/d/1TaGJjI0_Q1cENsfvbcCBlU3slaYx84fh/view',
+        permanent: false, // Use true for permanent (308) redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
