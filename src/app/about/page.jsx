0bced83a4fbd3d1ec36d36724967544da17135f7
emmanasseri <enasseri02@gmail.com>
2024-12-03 15:14:50 -0600

    final about changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,8 @@
 "use client";
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Heading, Text, Image, List } from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
 import { useState, useEffect } from "react";
+import { LuCheckCircle, LuCircleDashed } from "react-icons/lu";
 import Loading from "@/components/Loading";
 
 const About = () => {
@@ -34,21 +35,43 @@ const About = () => {
         fontSize="xl"
         pt={{ base: "20vh", md: "30vh" }}
       >
-        <Box display="flex" justifyContent="center" mv={4}>
+        <Box display="flex" justifyContent="center" mb={4}>
           <Image src="/ready_to_build.gif" alt="Ready to Build" />
         </Box>
-        <Heading size="4xl" mb={4} fontWeight="bold" fontStyle="italic">
+        <Heading size="4xl" my={6} mx={2} fontWeight="bold" fontStyle="italic">
           ABOUT
         </Heading>
-
-        <Text textAlign={"left"}>
+        <Text textAlign={"left"} mx={2} my={4}>
           The Midwest Block-a-Thon is a 20-hour blockchain-focused hackathon
           hosted at the University of Kansas aimed at fostering innovation and
           collaboration among students of all ages in the surrounding region.
-          This hackathon plans to bring in 200 hackers and community members
+          This hackathon plans to bring in{" "}
+          <Box as="span" fontWeight="bold">
+            200 hackers and community members
+          </Box>{" "}
           with hands-on experience in web3 development, mentorship from industry
           experts, and a chance to win big prizes.
         </Text>
+        <List.Root spacing={3} textAlign="left" mx={2} my={4}>
+          <List.Item mx={2} my={4}>
+            The Block-a-Thon will be hosted at the{" "}
+            <Box as="span" fontWeight="bold">
+              University of Kansas
+            </Box>
+            , where students will be welcome to stay overnight to hack.
+          </List.Item>
+          <List.Item mx={2} my={4}>
+            Meals and snacks will be provided throughout the event, and dietary
+            restrictions will be accommodated.
+          </List.Item>
+          <List.Item mx={2} my={4}>
+            This event is primarily focused on students, but community members
+            are welcome to attend as volunteers, mentors, or compete on the{" "}
+            <Box as="span" fontWeight="bold">
+              community track.
+            </Box>
+          </List.Item>
+        </List.Root>
       </Box>
     </div>
   );
