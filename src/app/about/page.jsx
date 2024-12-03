a523292d7f78c8fc73fb02445821a7bf5c9ec297
emmanasseri <enasseri02@gmail.com>
2024-12-03 14:11:25 -0600

    added gif on about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Image } from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
 import { useState, useEffect } from "react";
 import Loading from "@/components/Loading";
@@ -17,6 +17,7 @@ const About = () => {
       return () => window.removeEventListener("load", handleLoad);
     }
   }, []);
+
   if (isLoading) {
     return <Loading />;
   }
@@ -33,16 +34,20 @@ const About = () => {
         fontSize="xl"
         pt={{ base: "20vh", md: "30vh" }}
       >
-        <Heading size="3xl" mb={4} fontWeight="bold" fontStyle="italic">
+        <Box display="flex" justifyContent="center" mv={4}>
+          <Image src="/ready_to_build.gif" alt="Ready to Build" />
+        </Box>
+        <Heading size="4xl" mb={4} fontWeight="bold" fontStyle="italic">
           ABOUT
         </Heading>
+
         <Text textAlign={"left"}>
           The Midwest Block-a-Thon is a 20-hour blockchain-focused hackathon
           hosted at the University of Kansas aimed at fostering innovation and
           collaboration among students of all ages in the surrounding region.
-          This hackathon hopes to provide over 200 hackers with hands-on
-          experience in web3 development, mentorship from industry experts, and
-          a chance to win big prizes.
+          This hackathon plans to bring in 200 hackers and community members
+          with hands-on experience in web3 development, mentorship from industry
+          experts, and a chance to win big prizes.
         </Text>
       </Box>
     </div>
