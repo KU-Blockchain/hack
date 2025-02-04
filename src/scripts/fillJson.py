d1fc4871773c28ab1f9dadb175bd4fbb5de8b940
Micah Borghese <micahborghese@gmail.com>
2025-02-03 22:08:08 -0600

    Acceptance update 2

--- src/scripts/fillJson.py
+++ src/scripts/fillJson.py
@@ -1,1 +1,6 @@
 ## script to fill the json file with the data from the csv file
+import numpy as np
+
+applicant_data = np.loadtxt('applicant_data.csv', delimiter=',', dtype=str)
+
+print(applicant_data)
